75a5288b688e443c9fefb0089876834d5ee529c7
solutionxlbv <henk@solutionxl.nl>
2026-05-01 16:53:13 +0200

    Delete solutionxl/solutionxl 4 directory

--- solutionxl/solutionxl 4/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
-export default defineConfig({
-  site: 'https://www.solutionxl.nl',
-  integrations: [sitemap()],
-  build: { inlineStylesheets: 'auto' },
-  compressHTML: true,
-});
